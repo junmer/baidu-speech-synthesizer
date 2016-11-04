d2bba1a6bdfbc92e8aa437758b2ab3cc32c4a202
Lingjun.Meng <junmer@foxmail.com>
2016-11-04 23:56:42 +0800

    time out

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,7 +4,7 @@ describe('#synthesizer', function () {
         cuid: 'test'
     });
 
-    describe('##init', function () {
+    describe('init', function () {
 
         it('options should be assigned', function () {
             expect(bss.options.cuid).toBe('test');
@@ -13,9 +13,15 @@ describe('#synthesizer', function () {
     });
 
 
-    describe('##speak', function () {
+    describe('speak', function () {
 
+        var originalTimeout;
         var speaker = bss.speak('say something');
+
+        beforeEach(function() {
+            originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+        });
 
         it('speaker should has play', function () {
             expect(typeof speaker.play).toBe('function');
@@ -32,10 +38,13 @@ describe('#synthesizer', function () {
 
         });
 
+        afterEach(function() {
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+        });
+
     });
 
-
-    describe('##stop', function () {
+    describe('stop', function () {
         it('bss should has stop', function () {
             expect(typeof bss.stop).toBe('function');
         });
